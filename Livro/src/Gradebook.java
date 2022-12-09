0668880eb676c73fbd97329131df50573546e6dc
LucianoMRJ <120183320+LucianoMRJ@users.noreply.github.com>
2022-12-09 11:07:32 -0300

    Add files via upload

---
+++ Livro/src/Gradebook.java
@@ -0,0 +1,6 @@
+public class Gradebook {
+    public void displayMessage(){
+        System.out.println("Welcome to my gradebook");
+    }
+    
+}
---
+++ Livro/src/Teste.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Teste {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int r;
+        r = scan.nextInt();
+//        double Diâmetro = 2 * r,
+//            Circuferência = 2 * Math.PI * r,
+//            Área = Math.PI * Math.PI;
+        System.out.printf("Raio do círculo:%123\n", r);
+//        System.out.printf("Diâmetro do círculo: %2f\n", Diâmetro);
+//        System.out.printf("Circuferência do círculo: %2f\n", Circuferência);
+//        System.out.printf("Área do círculo: %2f\n", Área);
+        System.out.println("asd");
+        System.exit(0);
+    }
+}
